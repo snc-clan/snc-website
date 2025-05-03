48e998da73706622170b60c19adb4d2fa7651e7a
snc-clan <martinmwweiss64@gmail.com>
2025-05-03 11:50:25 +0200

    Add files via upload

---
+++ Registrierung/script.js
@@ -0,0 +1,26 @@
+document.addEventListener("contextmenu", (event) => event.preventDefault());
+document.addEventListener("keydown", (event) => {
+    if (event.key === "F12" || (event.ctrlKey && event.shiftKey && event.key === "I")) {
+        event.preventDefault();
+    }
+});
+
+function login() {
+    const allowedUsernames = ["CoOwner", "Supporter", "Moderator", "Anführer", "Admin", "Owner"];
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const errorDiv = document.getElementById('error');
+    const messageDiv = document.getElementById('message');
+    
+    if (allowedUsernames.includes(username) && password === "19143") {
+        errorDiv.textContent = "";
+        messageDiv.textContent = `Willkommen, ${username}!`;
+        messageDiv.style.display = "block";
+        
+        setTimeout(() => {
+            window.location.href = "../kalender/index.html";
+        }, 3000);
+    } else {
+        errorDiv.textContent = "Falscher Benutzername oder Passwort!";
+    }
+}
